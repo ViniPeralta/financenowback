8c9f4d155452be448f154750bbef781560c6ac1b
ViniciusPeralta <vinicius-peralta@hotmail.com>
2024-04-01 13:12:35 -0300

    Upgrading spring version

--- src/main/java/com/peralta/financenow/exception/ErrorResponse.java
+++ src/main/java/com/peralta/financenow/exception/ErrorResponse.java
@@ -1,24 +1,8 @@
 package com.peralta.financenow.exception;
 
-import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
-@Data
 @Builder
-@NoArgsConstructor
-@AllArgsConstructor
-public class ErrorResponse {
-
-    public String message;
-
-    public LocalDateTime dateTime;
-
-    public String method;
-
-    public String path;
-    
-}
+public record ErrorResponse(String message, LocalDateTime dateTime, String method, String path) {}
