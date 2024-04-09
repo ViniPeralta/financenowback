9fba15f67d4ad677d783d002fb9c5c85bea00544
ViniciusPeralta <vinicius-peralta@hotmail.com>
2024-04-08 21:09:46 -0300

    T02 - Initial Load

--- Scripts.sql
+++ Scripts.sql
@@ -23,22 +23,37 @@ create table tb_extract (
 
 create table tb_country (
 	id int auto_increment primary key,
-	name varchar(100),
-	abbreviation varchar(10)
+	name varchar(100) not null,
+	abbreviation varchar(10) not null
 );
+
+insert into tb_country (name, abbreviation) values ('Brazil', 'BR');
 
 create table tb_state (
 	id int auto_increment primary key,
-	name varchar(100),
-	abbreviation varchar(10),
-	country_id int,
+	name varchar(100) not null,
+	abbreviation varchar(10) not null,
+	country_id int not null,
 	foreign key (country_id) references tb_country(id)
 );
 
+insert into tb_state (name, abbreviation, country_id) values ('Sao Paulo', 'SP', 1);
+
 create table tb_city (
 	id int auto_increment primary key,
-	name varchar(100),
-	abbreviation varchar(10),
-	state_id int,
+	name varchar(100) not null,
+	abbreviation varchar(10) not null,
+	state_id int not null,
 	foreign key (state_id) references tb_state(id)
 );
+
+insert into tb_city (name, abbreviation, state_id) values
+('Sao Paulo', 'SP', 1),
+('Guarulhos', 'GRU', 1),
+('Campinas', 'CAM', 1),
+('Sao Bernardo do Campo', 'SBC', 1),
+('Santo Andre', 'SA', 1),
+('Osasco', 'OS', 1),
+('Ribeirao Preto', 'RP', 1),
+('Sao Jose dos Campos', 'SJC', 1),
+('Sao Jose do Rio Preto', 'SJRP', 1);
